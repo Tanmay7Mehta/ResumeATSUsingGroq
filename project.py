8e2705e022c96a0a964416304caddc7685175daf
Tanmay Mehta <10maymehta@gmail.com>
2026-08-09 15:01:28 +0530

    AIResponseFunction

--- project.py
+++ project.py
@@ -6,3 +6,16 @@ from groq import Groq
 
 load_dotenv()
 client = Groq(api_key=os.getenv("GROQ_API_KEY"))
+
+def gret_response(input_propmt, resume_text, job_des):
+    response = client.chat.completions.create(
+        model="llama-3.3-70b-versatile",
+        messages=[
+            {
+                "role": "user",
+                "content": input_propmt + "\n\n Resume Content:\n" + resume_text + "\n\n Job Description:\n" + job_des
+            }
+        ],
+        max_tokens=2048
+    )
+    return response.choice[0].message.content
