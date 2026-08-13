c108eae2273f54d7efafc4005673dee18f1d7b50
Tanmay Mehta <10maymehta@gmail.com>
2026-08-13 14:13:31 +0530

    AIResponseFunction

---
+++ test.py
@@ -0,0 +1,22 @@
+from dotenv import load_dotenv
+import streamlit as st
+import os
+import fitz
+from gorq import Groq
+
+load_dotenv()
+client = Groq(api_key=os.getenv("GROQ_API_KEY"))
+
+def get_response(input_prompt, resume_text, job_des):
+    response = client.chat.completions.create(
+        model = "llams-3.3-70b-versatile",
+        messages=[
+            {
+                "role": "user",
+                "content": input_prompt + "\n\nResume Content:\n" + resume_text + "\n\nJob Description:\n" + job_des
+            }
+        ],
+        max_token = 2048
+    )
+    return response.choices[0].message.content
+
