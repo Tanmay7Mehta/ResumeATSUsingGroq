062575e0ddcfd5ea5c8722ba4e372343b177f7df
Tanmay Mehta <10maymehta@gmail.com>
2026-08-16 14:11:47 +0530

    FixingTypos

--- test.py
+++ test.py
@@ -1,23 +1,22 @@
-from ResumeATSUsingGemini.project import submit1
 from dotenv import load_dotenv
 import streamlit as st
 import os
 import fitz
-from gorq import Groq
+from groq import Groq
 
 load_dotenv()
 client = Groq(api_key=os.getenv("GROQ_API_KEY"))
 
 def get_response(input_prompt, resume_text, job_des):
     response = client.chat.completions.create(
-        model = "llams-3.3-70b-versatile",
+        model = "llama-3.3-70b-versatile",
         messages=[
             {
                 "role": "user",
                 "content": input_prompt + "\n\nResume Content:\n" + resume_text + "\n\nJob Description:\n" + job_des
             }
         ],
-        max_token = 2048
+        max_tokens = 2048
     )
     return response.choices[0].message.content
 
